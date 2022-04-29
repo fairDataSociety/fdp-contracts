2976eb0980cb2c3833cd25fbf98d021062da6209
Vladan <tomic.vladan.mail@gmail.com>
2022-04-29 09:47:21 +0200

    fix: export models

--- js-library/src/index.ts
+++ js-library/src/index.ts
@@ -1,2 +1,3 @@
 export * from './constants'
 export * from './services/ens'
+export * from './model'
---
+++ js-library/src/model/index.ts
@@ -0,0 +1,3 @@
+export * from './ens-user-data.model'
+export * from './environment.enum'
+export * from './hex.types'
