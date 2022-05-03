80d645fe089fc2ee8ab0230c13d7489ffabb85b9
Vladan <tomic.vladan.mail@gmail.com>
2022-05-03 12:49:33 +0200

    ci: fix image build scripts

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -5,9 +5,9 @@ ROOT_PATH=$( cd "$ROOT_PATH/.." && pwd )
 # Getting env variables from bee-factory
 BEE_ENV_PREFIX=$(npm explore bee-factory -- ./scripts/utils/env-variable-value.sh BEE_ENV_PREFIX)
 BLOCKCHAIN_VERSION=$(npm explore bee-factory -- ./scripts/utils/env-variable-value.sh BLOCKCHAIN_VERSION)
-BLOCKCHAIN_CONTAINER_NAME="swarm-test-blockchain"
-CONTRACTS_IMAGE_NAME="$BLOCKCHAIN_CONTAINER_NAME-contracts"
-CONTRACTS_IMAGE_PREFIX="hub.docker.com/orgs/fairdatasociety/repositories/fdp-contracts"
+BLOCKCHAIN_CONTAINER_NAME="$BEE_ENV_PREFIX-blockchain"
+CONTRACTS_IMAGE_NAME="swarm-test-blockchain"
+CONTRACTS_IMAGE_PREFIX="hub.docker.com/orgs/fairdatasociety/repositories"
 CONTRACTS_IMAGE_URL="$CONTRACTS_IMAGE_PREFIX/$CONTRACTS_IMAGE_NAME:$BLOCKCHAIN_VERSION"
 ENV_FILE="$ROOT_PATH/dist/contracts.env"
 JS_LIB_CONTRACTS_DIR="$ROOT_PATH/js-library/src/contracts"
--- scripts/release-image.sh
+++ scripts/release-image.sh
@@ -5,9 +5,9 @@ ROOT_PATH=$( cd "$ROOT_PATH/.." && pwd )
 # Getting env variables from bee-factory
 BEE_ENV_PREFIX=$(npm explore bee-factory -- ./scripts/utils/env-variable-value.sh BEE_ENV_PREFIX)
 BLOCKCHAIN_VERSION=$(npm explore bee-factory -- ./scripts/utils/env-variable-value.sh BLOCKCHAIN_VERSION)
-BLOCKCHAIN_CONTAINER_NAME="swarm-test-blockchain"
-CONTRACTS_IMAGE_NAME="$BLOCKCHAIN_CONTAINER_NAME-contracts"
-CONTRACTS_IMAGE_PREFIX="hub.docker.com/orgs/fairdatasociety/repositories/fdp-contracts"
+BLOCKCHAIN_CONTAINER_NAME="$BEE_ENV_PREFIX-blockchain"
+CONTRACTS_IMAGE_NAME="warm-test-blockchain"
+CONTRACTS_IMAGE_PREFIX="hub.docker.com/orgs/fairdatasociety/repositories"
 CONTRACTS_IMAGE_URL="$CONTRACTS_IMAGE_PREFIX/$CONTRACTS_IMAGE_NAME:$BLOCKCHAIN_VERSION"
 
 docker push "$CONTRACTS_IMAGE_URL"
