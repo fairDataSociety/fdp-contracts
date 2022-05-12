48c228c1e5d3afb839e92fcc484bf12c0c8335e3
Rogelio Morrell <molekilla@gmail.com>
2022-05-12 08:37:57 -0500

    feat(contracts): remove unit tests not related to smart contracty

--- test/chunk.ts
+++ test/chunk.ts
@@ -14,35 +14,6 @@ describe('chunk', () => {
     await bmtlib.deployed()
   })
 
-  it('should initialise Chunk object', () => {
-    const chunk = makeChunk(payload)
-    const expectedSpan = new Uint8Array([3, 0, 0, 0, 0, 0, 0, 0])
-
-    expect(chunk.payload).equals(payload)
-    expect(chunk.span().byteLength).eq(expectedSpan.byteLength)
-    expect(chunk.data().length).equals(4096)
-    expect(chunk.address().length).equals(32)
-  })
-
-  it('should produce correct BMT hash', () => {
-    const hash = 'ca6357a08e317d15ec560fef34e4c45f8f19f01c372aa70f1da72bfa7f1a4338'
-    const chunk = makeChunk(payload)
-
-    const result = chunk.address()
-
-    expect(Utils.bytesToHex(result, 64)).equals(hash)
-  })
-
-  it('should test out bmtTree is in line with Chunk object calculations', () => {
-    const chunk = makeChunk(payload)
-    const tree = chunk.bmt()
-    expect(tree.length).equals(8)
-    const rootHash = tree[tree.length - 1]
-
-    let merged = Buffer.concat([chunk.span(), rootHash])
-    expect(ethers.utils.keccak256(merged).replace('0x', '')).equals(Utils.bytesToHex(chunk.address(), 64))
-  })
-
   it('should retrieve the required segment pairs for inclusion proof', async () => {
     const chunk = makeChunk(payload)
     const tree = chunk.bmt()
@@ -57,7 +28,7 @@ describe('chunk', () => {
     }
     const rootHash1 = await testGetRootHash(0)
 
-    let merged = Buffer.concat([makeSpan(payload.length), rootHash1])
+    const merged = Buffer.concat([makeSpan(payload.length), rootHash1])
     expect(keccak256(merged).replace('0x', '')).equals(Utils.bytesToHex(bmtHashOfPayload, 64))
     const rootHash2 = await testGetRootHash(101)
     expect(Utils.bytesToHex(rootHash2, 64)).equals(Utils.bytesToHex(rootHash1, 64))
