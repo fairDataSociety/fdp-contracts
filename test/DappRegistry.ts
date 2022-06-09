647cfef235a1e9fd7bc4cce059f687cd60de981a
Rogelio Morrell <molekilla@gmail.com>
2022-06-09 12:20:06 -0500

    feat(contracts):  unit tests

--- test/DappRegistry.ts
+++ test/DappRegistry.ts
@@ -1,13 +1,19 @@
-/* eslint-disable node/no-unsupported-features/es-syntax */
 import { expect } from 'chai'
 import { ethers } from 'hardhat'
-import { DappRegistry, ENSRegistry, SubdomainRegistrar, TestToken } from '../typechain'
+import { DappRegistry, ENSRegistry, FDSRegistrar, TestToken } from '../typechain'
+
+const ZERO_HASH = '0x0000000000000000000000000000000000000000000000000000000000000000'
+
+function sha3(value: string) {
+  return ethers.utils.keccak256(ethers.utils.toUtf8Bytes(value))
+}
 
 describe('dapp registry', () => {
   let dappRegistry: DappRegistry
   let testToken: TestToken
   let ens: ENSRegistry
-  let subdomainRegistry: SubdomainRegistrar
+  let registrar: FDSRegistrar
+
   const name = 'Swarm Dapp Registry'
   const amount = ethers.utils.parseEther('1')
   const record = {
@@ -16,18 +22,20 @@ describe('dapp registry', () => {
     description: 'NFT Sample Dapp',
     version: 1,
     indexType: 1,
-    dataFormat: ethers.utils.formatBytes32String('beeson'),
+    dataFormat: sha3('beeson'),
     blobRef: '0xfd79d5e0ebd8407e422f53ce1d7c4c41ebf403be55143900f8d1490560294780',
     timestamp: new Date().getTime(),
   }
   before(async () => {
+    const [, controllerAccount] = await ethers.getSigners()
     const ENSRegistry = await ethers.getContractFactory('ENSRegistry')
     ens = await ENSRegistry.deploy()
     await ens.deployed()
 
-    const SubdomainRegistrar = await ethers.getContractFactory('SubdomainRegistrar')
-    subdomainRegistry = await SubdomainRegistrar.deploy(ens.address, ethers.utils.formatBytes32String('fds'))
-    await subdomainRegistry.deployed()
+    const FDSRegistrar = await ethers.getContractFactory('FDSRegistrar')
+    registrar = await FDSRegistrar.deploy(ens.address)
+    await registrar.addController(controllerAccount.address)
+    await ens.setSubnodeOwner(ZERO_HASH, sha3('fds'), registrar.address)
 
     const TestToken = await ethers.getContractFactory('TestToken')
     testToken = await TestToken.deploy()
@@ -46,35 +54,28 @@ describe('dapp registry', () => {
   })
 
   describe('when creating a new record', () => {
-    beforeEach(async () => {
-      const [owner] = await ethers.getSigners()
-      await subdomainRegistry.register(ethers.utils.formatBytes32String('dapp.registry.fds'), owner.address)
-      const query = ens.filters.NewOwner(null, null, null)
-      const logs = await ens.queryFilter(query)
-      const log = logs[0].args
-      expect(log[1]).equal('dapp.registry.fds')
-      expect(log[2]).equal(owner)
+    before(async () => {
+      const [owner, controllerAccount] = await ethers.getSigners()
+
+      await registrar.connect(controllerAccount).register(sha3('dapp'), owner.address, 86400)
+      expect(await ens.owner(ethers.utils.namehash('dapp.fds'))).equal(owner.address)
+      expect(await registrar.ownerOf(sha3('dapp'))).equal(owner.address)
     })
 
     it('should add dapp record', async () => {
-      let query = ens.filters.NewOwner(null, null, null)
-      let logs = await ens.queryFilter(query)
-      const node = logs[0].args[0]
-      record.node = node
-
+      const node = ethers.utils.namehash('dapp.fds')
       const staked = ethers.utils.parseEther('1')
+      record.node = node
       await testToken.approve(dappRegistry.address, staked)
       await dappRegistry.add(node, staked, record)
-      query = dappRegistry.filters.DappRecordAdded(null, null, null)
-      logs = await dappRegistry.queryFilter(query)
+      const query = dappRegistry.filters.DappRecordAdded(null, null, null)
+      const logs = await dappRegistry.queryFilter(query)
       const log = logs[0].args
       expect(log[0]).equal(node)
     })
 
     it('should fetch dapp', async () => {
-      const query = ens.filters.NewOwner(null, null, null)
-      const logs = await ens.queryFilter(query)
-      const node = logs[0].args[0]
+      const node = ethers.utils.namehash('dapp.fds')
 
       const dapp = await dappRegistry.get(node)
       expect(dapp.node).equal(node)
