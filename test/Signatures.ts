8c361a9a75dfff8052e8d6c920e6d67354e7e63d
Viktor Levente Tóth <toth.viktor.levente@gmail.com>
2023-01-16 12:15:22 +0100

    test: single owner chunk signature recovery

--- test/Signatures.ts
+++ test/Signatures.ts
@@ -28,4 +28,36 @@ describe('Signatues', function () {
     // Incorrect message returns false
     expect(await postageStampSig.verify(signer.address, sig, chunkAddrHex, batchIdHex, '0', ts)).to.equal(false)
   })
+
+  it('should recover Single Owner Chunk signature', async function () {
+    const accounts = await ethers.getSigners()
+
+    const SocSig = await ethers.getContractFactory('SocSig')
+    const socSig = await SocSig.deploy()
+    await socSig.deployed()
+
+    const signer = accounts[0]
+    const topicHex = '0x0000000000000000000000000000000000000000000000000000000000000000'
+    const chunkAddrHex = '0x98371fb1297da62c1355abd8f9a7e43dd20cd5ddb9db7ba7c2d99e35e7afb58f'
+
+    const hash = await socSig.getMessageHash(topicHex, chunkAddrHex)
+    const sig = await signer.signMessage(ethers.utils.arrayify(hash))
+
+    const ethHash = await socSig.getEthSignedMessageHash(hash)
+
+    expect(signer.address).to.equal(await socSig.recoverSigner(ethHash, sig))
+
+    // Correct signature and message returns true
+    expect(await socSig.verify(signer.address, sig, topicHex, chunkAddrHex)).to.equal(true)
+
+    // Incorrect message returns false
+    expect(
+      await socSig.verify(
+        signer.address,
+        sig,
+        '0x0000000000000000000000000000000000000000000000000000000000000001',
+        chunkAddrHex,
+      ),
+    ).to.equal(false)
+  })
 })
