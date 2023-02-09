7593c2b8fc22aeeff6c756677bc0b1810d87f110
tomicvladan <tomic.vladan.mail@gmail.com>
2023-02-09 16:30:59 +0100

    fix: script for checking changes in contracts

--- scripts/get-changes.sh
+++ scripts/get-changes.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# Checks which sets of contracts have been changed since previous commit
+# Checks which sets of contracts have been changed since previous release
 # Example return "ENS BMT DAPP_REGISTRY"
 
 OUTPUT=""
-CHANGED_FILES=$(git diff --name-only HEAD HEAD~1)
+LAST_RELEASE_COMMIT_HASH=$(git log --all -n 1 --grep='chore(master): release contracts' --pretty=format:"%h")
+CHANGED_FILES=$(git diff --name-only HEAD $LAST_RELEASE_COMMIT_HASH)
 
 # ENS contracts changed
 if [[ "$CHANGED_FILES" =~ "ENSRegistry.sol" || "$CHANGED_FILES" =~ "FDSRegistrar.sol" || "$CHANGED_FILES" =~ "PublicResolver.sol" ]];
